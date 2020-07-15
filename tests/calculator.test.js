f4aa76504bed228ee6d9ee6094cb44ca70174e11
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-14 21:24:02 -0400

    Adicionei casos de teste com números negativos

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -11,11 +11,14 @@ describe('Quando o módulo calculator for importado', () => {
 
 describe('Quando utilizarmos a operação de soma()', () => {
     test('Deve somar dois números e retornar os valores corretos', () => {
+        expect(calculator.sum(-2, -5)).toBe(-7);
+        expect(calculator.sum(-1, -4)).toBe(-5);
+        expect(calculator.sum(-2, -1)).toBe(-3);
+        expect(calculator.sum(-2, 0)).toBe(-2);
+        expect(calculator.sum(-1, 1)).toBe(0);
         expect(calculator.sum(1, 1)).toBe(2);
         expect(calculator.sum(2, 1)).toBe(3);
         expect(calculator.sum(3, 2)).toBe(5);
         expect(calculator.sum(5, 2)).toBe(7);
-        expect(calculator.sum(1, 10)).toBe(11);
-        expect(calculator.sum(6, 7)).toBe(13);
     });
 });
