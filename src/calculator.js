298e81f1ff2239bb5a255904aecc88b3240c4efc
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-14 21:03:52 -0400

    Criei a estrutura dos métodos da calculadora

---
+++ src/calculator.js
@@ -0,0 +1,21 @@
+class Calculator {
+    sum() {
+        // TODO: needs implementation
+    }
+
+    subtract() {
+        // TODO: needs implementation
+    }
+
+    multiply() {
+        // TODO: needs implementation
+    }
+
+    divide() {
+        // TODO: needs implementation
+    }
+}
+
+const calculator = new Calculator();
+
+module.exports = calculator;
