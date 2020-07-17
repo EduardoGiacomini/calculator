611305f70f2dce4be47f6a64a0f497111364fe93
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-16 21:11:10 -0400

    Fiz correções nas mensagens dos testes

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -10,7 +10,7 @@ describe('Quando o módulo calculator for importado', () => {
 });
 
 describe('Quando utilizarmos a operação sum()', () => {
-    test('Deve somar dois números e retornar o valor correto', () => {
+    test('Deve somar dois números e retornar o resultado', () => {
         expect(calculator.sum(-20, -5)).toBe(-25);
         expect(calculator.sum(-1, 1)).toBe(0);
         expect(calculator.sum(3, 2)).toBe(5);
@@ -18,7 +18,7 @@ describe('Quando utilizarmos a operação sum()', () => {
 });
 
 describe('Quando utilizarmos a operação subtract()', () => {
-    test('Deve subtrair o primeiro valor pelo segundo e retornar o valore correto', () => {
+    test('Deve subtrair o primeiro valor pelo segundo e retornar o resultado', () => {
         expect(calculator.subtract(-10, -3)).toBe(-7);
         expect(calculator.subtract(1, 1)).toBe(0);
         expect(calculator.subtract(100, 83)).toBe(17);
@@ -26,7 +26,7 @@ describe('Quando utilizarmos a operação subtract()', () => {
 });
 
 describe('Quando utilizarmos a operação multiply()', () => {
-    test('Deve multiplicar dois valores e retornar o valore correto', () => {
+    test('Deve multiplicar dois valores e retornar o resultado', () => {
         expect(calculator.multiply(3, -5)).toBe(-15);
         expect(calculator.multiply(0, 2)).toBe(0);
         expect(calculator.multiply(-9, -100)).toBe(900);
@@ -40,7 +40,7 @@ describe('Quando utilizarmos a operação divide()', () => {
         expect(() => calculator.divide(17, 0)).toThrow();
     });
 
-    test('Deve dividir dois valores e retornar o valor correto', () => {
+    test('Deve dividir o primeiro valor pelo segundo e retornar o resultado', () => {
         expect(calculator.divide(-15, 5)).toBe(-3);
         expect(calculator.divide(10, -2)).toBe(-5);
         expect(calculator.divide(14, 2)).toBe(7);
