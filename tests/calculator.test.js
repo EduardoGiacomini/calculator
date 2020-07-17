96dc45bcfd73c8baf20a7b2025f384f7b479d7a3
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-07-16 21:28:23 -0400

    Criei os casos de testes para o fatorial

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,11 +1,12 @@
 const calculator = require('../src/calculator');
 
 describe('Quando o módulo calculator for importado', () => {
-    test('Deve possuir as operações sum(), subtract(), multiply(), divide()', () => {
+    test('Deve possuir as operações sum(), subtract(), multiply(), divide(), factorial()', () => {
         expect(calculator).toHaveProperty('sum');
         expect(calculator).toHaveProperty('subtract');
         expect(calculator).toHaveProperty('multiply');
         expect(calculator).toHaveProperty('divide');
+        expect(calculator).toHaveProperty('factorial');
     });
 });
 
@@ -44,5 +45,19 @@ describe('Quando utilizarmos a operação divide()', () => {
         expect(calculator.divide(-15, 5)).toBe(-3);
         expect(calculator.divide(10, -2)).toBe(-5);
         expect(calculator.divide(14, 2)).toBe(7);
+    });
+});
+
+describe('Quando utilizarmos a operação factorial()', () => {
+    test('Deve tentar realizar o fatorial de um número negativo e retornar uma exceção', () => {
+        expect(() => calculator.factorial(-1)).toThrow();
+        expect(() => calculator.factorial(-10)).toThrow();
+        expect(() => calculator.factorial(-95)).toThrow();
+    });
+
+    test('Deve calcular o fatorial de um número e retornar o resultado', () => {
+        expect(calculator.factorial(3)).toBe(6);
+        expect(calculator.factorial(7)).toBe(5040);
+        expect(calculator.factorial(13)).toBe(6_227_020_800);
     });
 });
